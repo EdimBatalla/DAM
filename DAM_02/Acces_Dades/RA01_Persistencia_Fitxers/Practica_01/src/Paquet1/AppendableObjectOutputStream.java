83124a56b1a4482c12576470009fa02bf4a4eab8
Edim <edim.batalla@gmail.com>
2025-10-14 20:53:18 +0200

    actualizando

---
+++ DAM_02/Acces_Dades/RA01_Persistencia_Fitxers/Practica_01/src/Paquet1/AppendableObjectOutputStream.java
@@ -0,0 +1,18 @@
+package Paquet1;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+public class AppendableObjectOutputStream extends ObjectOutputStream {
+public AppendableObjectOutputStream(OutputStream out) throws
+IOException {
+super(out);
+}
+
+@Override
+protected void writeStreamHeader() throws IOException {
+// No escrivim la capçalera quan fem append
+reset();
+}
+}
